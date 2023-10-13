956df1f5553ea4a8e103026ae77d76d6d1cc4f85
Matthew <defender_0000@mail.ru>
2023-10-13 13:49:39 +0300

    Initial commit

---
+++ src/main/java/ru/accelerator/FranchHub/FranchHubApplication.java
@@ -0,0 +1,13 @@
+package ru.accelerator.FranchHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FranchHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FranchHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/accelerator/FranchHub/FranchHubApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.accelerator.FranchHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranchHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
