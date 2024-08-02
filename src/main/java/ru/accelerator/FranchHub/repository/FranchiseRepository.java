357e1c157ad95d09742076beee1abb57628d21b4
Nikolay <kolxz2001@gmail.com>
2024-08-02 08:45:32 +0300

    Загрузка и выгрузка фотографий на сервер.

--- src/main/java/ru/accelerator/FranchHub/repository/FranchiseRepository.java
+++ src/main/java/ru/accelerator/FranchHub/repository/FranchiseRepository.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 import ru.accelerator.FranchHub.entity.FranchiseEntity;
 
 @Repository
-public interface FranchiseRepository extends CrudRepository<FranchiseEntity, Integer> {
+public interface FranchiseRepository extends CrudRepository<FranchiseEntity, Integer>, CategoryRepository {
     FranchiseEntity findById (int id);
     FranchiseEntity findByTitle (String title);
 }
